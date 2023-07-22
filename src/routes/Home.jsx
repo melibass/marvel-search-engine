8dc6e6947600652b000016d072363fa272730074
melibass <mbassano@gmail.com>
2023-07-22 15:49:33 +0200

    feat: thumbnail issue to fix

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,34 +5,31 @@ import Container from '../components/Container';
 import SearchBar from '../components/SearchBar';
 import Grid from '../components/Grid';
 import Card from '../components/Card';
-import { hasUnreliableEmptyValue } from '@testing-library/user-event/dist/utils';
 
-const IMAGE_CHOSEN = 'portrait_xlarge'
+const IMAGE_CHOSEN = 'portrait_fantastic'
 export default function Home() {
     // heroes es la variable, setHeroes es la funcion que uso para modificarlo.
     const [ heroes, setHeroes ] = useState([]);
 
     let cards;
 
-    if(heroes) {
-      cards = heroes.map((hero)=>{
-        < Card 
+    if(heroes){
+      cards = heroes.map((hero)=> 
+        <Card 
           name={hero.name}
           id={hero.id}
           key={hero.id}
           thumbnail={`${hero.thumbnail.path}/${IMAGE_CHOSEN}.${hero.thumbnail.extension}`}
         />
-      })
+      )
     }
+
   return (
     <Container>
         <h1>Discover Marvel Universe</h1>
         <SearchBar setter={setHeroes}/>
             <Grid>
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-                
+                {cards ? cards : ''}
             </Grid>
             
     </Container>
